2de679d35b4adb6734440c28b1066b0521c62432
Pablo Maldonado <pablo.maldonado@utah.edu>
2024-12-16 15:06:56 -0600

    removed error logs

--- inference_run.sh
+++ inference_run.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --gpus=1
-#SBATCH --mem=64G
+#SBATCH --mem=200G
 #SBATCH --time=2:00:00
 #SBATCH --account=bdhi-delta-gpu
 
@@ -16,15 +16,6 @@ source /sw/external/python/anaconda3/etc/profile.d/conda.sh
 
 # Activate the Conda environment
 conda activate /u/pmaldonadocatala/.conda/envs/ml_env_2
-
-# Confirm the environment is activated
-echo "=== ENVIRONMENT ACTIVATION CHECK ==="
-echo "Using Python: $(which python)"
-echo "Python Version: $(python --version)"
-echo "Conda Environment: $(conda info --envs | grep '*')"
-
-# Confirm Torch is Available
-python -c "import torch; print('Torch Version:', torch.__version__)"
 
 # Run the inference script
 srun python inference_run.py
