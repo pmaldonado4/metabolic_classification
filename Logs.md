8b4e7e7b99c21c90cdf5a11c4ca60c9fc44cf631
Pablo Maldonado <pablo.maldonado@utah.edu>
2024-12-17 16:11:02 -0600

    added additional scripts to run embeddings

---
+++ Logs.md
@@ -0,0 +1,3 @@
+Logs:
+
+Dec 17th, ran inference to obtain embeddings of ~800k sequences. Split dataset into four chunks due to memory issues. Ran each individually with its on slurm and python script.
---
+++ inference_run_3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="hyena_inference"
+#SBATCH --output="x_inference_%j.out"
+#SBATCH --error="x_inference_%j.err"
+#SBATCH --partition=gpuA40x4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus=1
+#SBATCH --mem=200G
+#SBATCH --time=12:00:00
+#SBATCH --account=bdhi-delta-gpu
+
+# Source Conda environment setup
+source /sw/external/python/anaconda3/etc/profile.d/conda.sh
+
+# Activate the Conda environment
+conda activate /u/pmaldonadocatala/.conda/envs/ml_env_2
+
+# Run the inference script
+srun python inference_run_3.py
